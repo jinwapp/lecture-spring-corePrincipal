f1c8c9a3a85e99a3daf11258ac1f3fe6f09a7162
jinwapp <jinwaap@gmail.com>
2022-06-23 02:25:35 +0900

    update: 2022-06-23_02:25

--- src/main/java/HelloLombok.java
+++ src/main/java/HelloLombok.java
@@ -1,2 +1,39 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdfas");
+
+        System.out.println("helloLombok = " + helloLombok);
+
+    }
 }
+/*
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+*/
+
+
